096458626ac331f16774ee97483f3c26bede974f
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-24 13:36:45 -0400

    implement Hero Section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Navbar from '@/app/components/Navbar'
-export default function Layout({ childern }: Readonly<{ childern: React.ReactNode }>) {
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return(
         <main className="font-work-sans">
             <Navbar/>
-            {childern}
+            {children}
         </main>
     ) 
         
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
-import Image from "next/image";
 export default function Home() {
   return (
     <>
-      HOME
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, Get Noticed in Virtual Competition.</p>
+        
+      </section>
     </>
   );
 }
