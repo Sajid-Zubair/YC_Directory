b28d21c8a030f17446271d7aeff9edf445a30e22
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-24 14:00:24 -0400

    implement Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import { Search } from "lucide-react";
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({ searchParams}: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query
   return (
     <>
       <section className="pink_container">
@@ -7,7 +13,7 @@ export default function Home() {
           Connect With Entrepreneurs
         </h1>
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, Get Noticed in Virtual Competition.</p>
-        
+        <SearchForm query={query} />
       </section>
     </>
   );
