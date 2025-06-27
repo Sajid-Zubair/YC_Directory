7b507cef3b382f637a9a0434c38be8f99aa79fbc
Sajid-Zubair <sajid10zubair@gmail.com>
2025-06-27 18:23:36 -0400

    update unwanted imports

--- components/View.tsx
+++ components/View.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Ping from './Ping'
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
-import { writeClient } from '@/sanity/lib/write-client';
 
 const View = async ({ id }: {id : string}) => {
   const {views : totalViews} = await client
